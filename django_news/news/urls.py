2be8565f76d89475d05e699086faeee1a2be089d
mfjmori <47058332+mfjmori@users.noreply.github.com>
2019-09-11 16:09:24 +0900

    update news_index to transition page for each category

--- django_news/news/urls.py
+++ django_news/news/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
-  path('', views.news_index, name='news_index'),
+  path('', RedirectView.as_view(url='business')),
+  path('<str>/', views.news_index, name='news_index'),
 ]
--- django_news/news/views.py
+++ django_news/news/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . import urls
 import requests, os
 
-def news_index(request):
+def news_index(request, str):
   NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
   NEWS_API_URL = os.environ.get('NEWS_API_URL')
-  response_res = requests.get(NEWS_API_URL, {'country' : 'jp', 'category': 'business', 'apiKey' : NEWS_API_KEY})
+  categories = ['', 'business', 'science', 'technology']
+  if str in categories:
+    category = str
+  else:
+    return redirect('news_index', str='business')
+  response_res = requests.get(NEWS_API_URL, {'country' : 'jp', 'category': category, 'apiKey' : NEWS_API_KEY})
   response_dict = response_res.json()
   return render(request, 'news/news_index.html', { 'articles' : response_dict['articles']})
