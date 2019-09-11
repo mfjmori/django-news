51877378a69ace661c957b23541dbeb0898c357f
mfjmori <47058332+mfjmori@users.noreply.github.com>
2019-09-11 14:28:20 +0900

    create news_index

--- django_news/news/views.py
+++ django_news/news/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+import requests, os
 
-# Create your views here.
+def news_index(request):
+  NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+  NEWS_API_URL = os.environ.get('NEWS_API_URL')
+  response_res = requests.get(NEWS_API_URL, {'country' : 'jp', 'category': 'business', 'apiKey' : NEWS_API_KEY})
+  response_dict = response_res.json()
+  return render(request, 'news/news_index.html', { 'articles' : response_dict['articles']})
