b834e8015527f2fdab7b955b96d75433d8936401
mfjmori <47058332+mfjmori@users.noreply.github.com>
2019-09-12 17:50:06 +0900

    update Stock model

--- django_news/news/models.py
+++ django_news/news/models.py
@@ -4,9 +4,9 @@ from django.utils import timezone
 class Stock(models.Model):
   user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
   url = models.URLField()
-  image_url = models.URLField(null=True)
+  image_url = models.URLField(null=True, blank=True)
   title = models.CharField(max_length=100)
-  body = models.TextField(null=True)
+  body = models.TextField(null=True, blank=True)
   source = models.CharField(null=True, max_length=50)
   likes_count = models.IntegerField(null=True, blank=True)
   publish_at = models.DateTimeField()
