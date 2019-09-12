c66b6839e7b30bbc9faa07c49ff093a4d284519c
mfjmori <47058332+mfjmori@users.noreply.github.com>
2019-09-12 15:31:14 +0900

    add paese_date filter

---
+++ django_news/news/templatetags/filter.py
@@ -0,0 +1,7 @@
+from django import template
+import datetime
+
+register = template.Library()
+@register.filter(expects_localtime=True)
+def parse_date(value):
+  return datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%SZ")
