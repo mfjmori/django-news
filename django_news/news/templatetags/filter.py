489fdfdec6b9a8c1513648fce2bbaf41e53428ba
mfjmori <47058332+mfjmori@users.noreply.github.com>
2019-09-12 17:50:23 +0900

    Update filter.py

--- django_news/news/templatetags/filter.py
+++ django_news/news/templatetags/filter.py
@@ -1,7 +1,8 @@
 from django import template
 import datetime
+import dateutil.parser
 
 register = template.Library()
 @register.filter(expects_localtime=True)
 def parse_date(value):
-  return datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%SZ")
+  return dateutil.parser.parse(value)
