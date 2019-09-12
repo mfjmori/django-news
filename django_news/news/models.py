4999b956fdac537aca98c82c88282b3384da4496
mfjmori <47058332+mfjmori@users.noreply.github.com>
2019-09-12 15:30:22 +0900

    add stock model

--- django_news/news/models.py
+++ django_news/news/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Stock(models.Model):
+  user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+  url = models.URLField()
+  image_url = models.URLField(null=True)
+  title = models.CharField(max_length=100)
+  body = models.TextField(null=True)
+  source = models.CharField(null=True, max_length=50)
+  likes_count = models.IntegerField(null=True, blank=True)
+  publish_at = models.DateTimeField()
+
+  class Meta:
+    unique_together = ['user', 'url']
