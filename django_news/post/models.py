91330da1a270d027e1c747423b97ababd9ceffb8
mfjmori <47058332+mfjmori@users.noreply.github.com>
2019-09-13 13:09:25 +0900

    add post model

--- django_news/post/models.py
+++ django_news/post/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+  author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+  title = models.CharField(max_length=50)
+  body = models.TextField()
+  publish_at = models.DateTimeField()
