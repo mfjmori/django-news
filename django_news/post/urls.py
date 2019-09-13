d71a98fd20a17ebd974e4f25ca79167fd8c9a2bb
mfjmori <47058332+mfjmori@users.noreply.github.com>
2019-09-13 13:52:28 +0900

    create post_show

--- django_news/post/urls.py
+++ django_news/post/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
   path('', views.post_index, name='post_index'),
   path('new/', views.post_new, name='post_new'),
+  path('show/<int:pk>', views.post_show, name='post_show'),
 ]
--- django_news/post/views.py
+++ django_news/post/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import CreateView
 from django.utils import timezone
 from .models import Post
@@ -19,8 +19,12 @@ def post_new(request):
       post.author = request.user
       post.publish_at = now
       post.save()
-      return redirect('post_index')
+      return redirect('post_show', pk=post.pk )
   else:
     form = PostForm()
   return render(request, 'post/post_new.html', {'form': form} )
 
+@login_required
+def post_show(request, pk):
+  post = get_object_or_404(Post, pk=pk)
+  return render(request, 'post/post_show.html', {'post': post})
