01c55606b48c4777bf5354f415418c65f761a176
mfjmori <47058332+mfjmori@users.noreply.github.com>
2019-09-18 13:54:25 +0900

    create model scraping

--- django_news/scraping/models.py
+++ django_news/scraping/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Scraping(models.Model):
+  title = models.CharField(max_length=50)
+  body = models.TextField()
+  image = models.ImageField(upload_to='images', null=True, blank=True)
+  publish_at = models.DateTimeField()
+  url = models.URLField(unique=True)
