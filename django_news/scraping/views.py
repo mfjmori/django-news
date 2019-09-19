59e3ed41a024f26ae948aecfbed72e0e123eee83
mfjmori <47058332+mfjmori@users.noreply.github.com>
2019-09-19 17:37:57 +0900

    fix scraping\views.py

--- django_news/scraping/views.py
+++ django_news/scraping/views.py
@@ -5,15 +5,14 @@ from .models import Scraping
 def scraping_index(request):
   scrapings = Scraping.objects.order_by('publish_at').reverse()
   page = Paginator(scrapings, 25)
+  num = 1
   if request.GET.get('p'):
     num = int(request.GET.get('p'))
-  else:
-    num = None
 
   params = {
-      'articles': page.get_page(num),
-      'page': page.page_range,
-      'page_active': num,
-      'page_last': page.num_pages
-    }
+    'articles': page.get_page(num),
+    'page': page.page_range,
+    'page_active': num,
+    'page_last': page.num_pages
+  }
   return render(request, 'scraping/scraping_index.html', params )
