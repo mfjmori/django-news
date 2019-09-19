0ce85fa4fb1fe6ca778df81b34302e788bfb392a
mfjmori <47058332+mfjmori@users.noreply.github.com>
2019-09-19 13:44:49 +0900

    add pagination

--- django_news/scraping/views.py
+++ django_news/scraping/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import Scraping
 
 def scraping_index(request):
-  Scrapings = Scraping.objects.order_by('publish_at').reverse()
-  return render(request, 'scraping/scraping_index.html', {'articles' : Scrapings})
+  scrapings = Scraping.objects.order_by('publish_at').reverse()
+  page = Paginator(scrapings, 25)
+  if request.GET.get('p'):
+    num = int(request.GET.get('p'))
+  else:
+    num = None
+
+  params = {
+      'articles': page.get_page(num),
+      'page': page.page_range,
+      'page_active': num,
+      'page_last': page.num_pages
+    }
+  return render(request, 'scraping/scraping_index.html', params )
